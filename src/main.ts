ca4de9a6f9171897d519a021e5599bf3d502708b
Haesung <koh1260@naver.com>
2025-03-28 22:31:12 +0900

    chore: cors 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true });
+  app.enableCors({ origin: true, credentials: true });
   app.setGlobalPrefix('admin');
   app.useGlobalPipes(
     new ValidationPipe({
