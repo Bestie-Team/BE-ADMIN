b1c47a5c28ff637f82952c359b6ce9248ad6fe61
Haesung <koh1260@naver.com>
2025-03-28 22:24:41 +0900

    chore: 쿠키 proxy 설정

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ async function bootstrap() {
       resave: false,
       saveUninitialized: false,
       rolling: true,
+      proxy: true,
       cookie: {
         maxAge: 1000 * 60 * 5,
         secure: process.env.NODE_ENV === 'production' ? true : false,
